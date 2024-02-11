9f61a095f22f039f35770e03b215c18e0a36f116
Ryan Biondo <biondoryan.dev@gmail.com>
2024-02-11 11:30:10 -0700

    Better array Data output

--- app/games/new/NewGameForm.tsx
+++ app/games/new/NewGameForm.tsx
@@ -1,50 +1,21 @@
 'use client';
 
-import { Box, Button, Flex, Grid, Heading, TextField } from '@radix-ui/themes';
-import React, { useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { Box, Button, Flex, Grid, Heading } from '@radix-ui/themes';
+import React, { useEffect, useState } from 'react';
+import { useForm, Controller, useFieldArray } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import Select from 'react-select';
-import { z } from 'zod';
 
 interface Option {
   value: string;
   label: string;
 }
 interface FormData {
-  players: {
-    [key: string]: Option;
-  };
-  commanders: {
-    [key: string]: Option;
-  };
+  playerCommanderPairs: { player: Option; commander: Option }[];
   gameDescriptor: Option;
   winner: Option;
 }
-
-// export const gameFormSchema = z.object({
-//   players: z.array(z.string()).nonempty('At least one player is required.'),
-//   commanders: z
-//     .array(z.string())
-//     .nonempty('At least one commander is required.'),
-//   gameDescriptor: z.enum([
-//     'QuickMatch',
-//     'Marathon',
-//     'Upset',
-//     'CloseCall',
-//     'Dominance',
-//     'Chaos',
-//   ]),
-//   winner: z.string(),
-// });
-// type GameFormData = z.infer<typeof gameFormSchema>;
-
-// interface SelectProps<Option = unknown> {
-//   options: Option[];
-//   value: Option;
-//   onChange: (value: Option) => void;
-// }
 
 const playerOptions = [
   { value: 'player1', label: 'Player 1' },
@@ -70,52 +41,57 @@ const commanders = [
   { value: 'commander5', label: 'Karn, Silver Golem' },
   { value: 'commander6', label: 'Teysa Karlov' },
 ];
+const initialPair = {
+  player: { value: '', label: '' },
+  commander: { value: '', label: '' },
+};
 
 const NewGameForm = () => {
   const router = useRouter();
-  const { control, handleSubmit, setValue, getValues, reset } =
-    useForm<FormData>();
-
-  const [playerCount, setPlayerCount] = useState(4);
-  const addPlayer = () => setPlayerCount((prevCount) => prevCount + 1);
-  const removePlayer = () => {
-    if (playerCount > 1) {
-      setPlayerCount((prevCount) => prevCount - 1);
-    }
-  };
-
-  const onSubmit = handleSubmit(async (data) => {
-    // const result = gameFormSchema.safeParse(data);
-    // if (!result.success) {
-    //   console.error('Validation failed', result.error);
-    //   return;
-    // }
-
-    // // Data is validated and can be used
-    // const validData = result.data;
-
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      playerCommanderPairs: Array(4).fill(initialPair),
+      gameDescriptor: { value: '', label: '' },
+      winner: { value: '', label: '' },
+    },
+  });
+  const onSubmit = async (data: FormData) => {
     console.log(data);
     const payload = {
-      players: Object.values(data.players).map((player) => player.value),
-      commanders: Object.values(data.commanders).map(
-        (commander) => commander.value
-      ),
+      players: data.playerCommanderPairs.map((pair) => pair.player.value),
+      commanders: data.playerCommanderPairs.map((pair) => pair.commander.value),
       gameDescriptor: data.gameDescriptor.value,
       winner: data.winner.value,
     };
 
     try {
-      console.log('Submitting game:', payload);
       await axios.post('/api/games', payload);
-      console.log('Game created successfully');
       router.push('/games');
     } catch (error) {
       console.error('Error submitting game:', error);
     }
-  });
+  };
+
+  const [pairs, setPairs] = useState(Array(4).fill(initialPair));
+
+  const addPair = () => setPairs((pairs) => [...pairs, initialPair]);
+  const removePair = () => setPairs((pairs) => pairs.slice(0, -1));
+
+  // Reset form with updated pairs, gameDescriptor, and winner
+  const updateForm = () => {
+    reset({
+      playerCommanderPairs: pairs,
+      gameDescriptor: { value: '', label: '' },
+      winner: { value: '', label: '' },
+    });
+  };
+  // Invoke updateForm whenever pairs change to ensure form reflects the state
+  useEffect(() => {
+    updateForm();
+  }, [pairs]);
 
   return (
-    <form className="space-y-3" onSubmit={onSubmit}>
+    <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
       <Box>
         <Flex gap="2" my="4" justify="center">
           <Heading as="h2" mb="2" size="4">
@@ -131,27 +107,31 @@ const NewGameForm = () => {
         <Heading as="h3" mb="2" size="3">
           Players:
         </Heading>
-
-        {[...Array(playerCount)].map((_, index) => (
+        {pairs.map((_, index) => (
           <Grid key={index} columns="3" width="100%" gap="5" mb="6">
+            {/* Player field */}
             <Controller
-              name={`players.player${index + 1}`}
+              name={`playerCommanderPairs.${index}.player`}
               control={control}
               render={({ field }) => (
                 <Select
-                  className="basic-single"
-                  classNamePrefix="select"
-                  placeholder="Add a Player..."
-                  isClearable
-                  options={playerOptions}
                   {...field}
+                  options={playerOptions}
+                  placeholder="Select a Player..."
                 />
               )}
             />
+            {/* Commander field, currently assuming single commander setup */}
             <Controller
-              name={`commanders.commander${index + 1}`}
+              name={`playerCommanderPairs.${index}.commander`}
               control={control}
-              render={({ field }) => <Select options={commanders} {...field} />}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={commanders}
+                  placeholder="Select a Commander..."
+                />
+              )}
             />
 
             {/* Placeholder for the commander image */}
@@ -161,21 +141,16 @@ const NewGameForm = () => {
       </div>
 
       <Flex gap="5" mb="6" mr="9" justify="center">
+        <Button size="3" type="button" onClick={addPair}>
+          Add Player
+        </Button>
         <Button
           size="3"
           variant="soft"
-          onClick={removePlayer}
           type="button"
-          disabled={playerCount <= 2}>
+          onClick={removePair}
+          disabled={pairs.length <= 2}>
           Remove Player
-        </Button>
-
-        <Button
-          size="3"
-          onClick={addPlayer}
-          type="button"
-          disabled={playerCount >= 6}>
-          Add Player
         </Button>
       </Flex>
 
